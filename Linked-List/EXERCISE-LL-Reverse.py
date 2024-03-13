c49fd1c0f981e566e24951ae87dd20d11ab4b808
Dimitri <dimitri.legaspi@gmail.com>
2024-03-12 20:18:30 -0400

    Reversed the LL

---
+++ Linked-List/EXERCISE-LL-Reverse.py
@@ -0,0 +1,169 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while (temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        pre = self.get(index - 1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
+    def reverse(self):
+        # set temp to the head of the LL
+        temp = self.head
+        # switch the head with the tail
+        self.head = self.tail
+        # set the tail to temp
+        self.tail = temp
+        # set after variable to the next node after the head
+        after = temp.next
+        # we set before variable to None, none will essentially
+        # be before the head
+        before = None
+        # iterate through the length of the LL
+        for _ in range(self.length):
+            # set after to temp.next, which is the arrow pointing toward the
+            # other node
+            after = temp.next
+        # set temp.next to before, which turns the head to point in the
+        # other direction
+            temp.next = before
+        # we set before to temp so that it is set to the next node
+            before = temp
+        # set temp to after which moves across the gap that is made
+            temp = after
+        # at the end of the iteration after will be equal to None
+        # and then temp.next will be set to the last node
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+
+print('LL before reverse():')
+my_linked_list.print_list()
+
+my_linked_list.reverse()
+
+print('\nLL after reverse():')
+my_linked_list.print_list()
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    LL before reverse():
+    1
+    2
+    3
+    4
+
+    LL after reverse():
+    4
+    3
+    2
+    1
+
+"""
